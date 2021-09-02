624b895aeef81955a75b65ab52e3938edacd25ee
李宏超 <87232660+lihongchao-o@users.noreply.github.com>
2021-09-02 19:23:37 +0800

    namespace

---
+++ namespace/nsp.cpp
@@ -0,0 +1,11 @@
+#include "nsp.hpp"
+
+using namespace MyNamespace;
+
+int main() {
+
+  //MyNamespace::Print hchao(1);     // without using namespace MyNamespace
+  Print hchao;                   //  with                  -//- 
+
+  return 0;
+}
